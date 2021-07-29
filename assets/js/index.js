8780434938af2e487ad641c2771e0cd949b0ed3a
jimmy <worktien@gmail.com>
2021-07-29 12:55:03 +0800

    disable search feature for now

--- assets/js/index.js
+++ assets/js/index.js
@@ -55,111 +55,109 @@ function suggestionFocus(e){
 
 }
 
-
-/*
-Source:
-  - https://github.com/nextapps-de/flexsearch#index-documents-field-search
-  - https://raw.githack.com/nextapps-de/flexsearch/master/demo/autocomplete.html
-*/
-
-(function(){
-
-  var index = new FlexSearch.Document({
-    tokenize: "forward",
-    cache: 100,
-    document: {
-      id: 'id',
-      store: [
-        "href", "title", "description"
-      ],
-      index: ["title", "description", "content"]
-    }
-  });
-
-
-  // Not yet supported: https://github.com/nextapps-de/flexsearch#complex-documents
-
-  /*
-  var docs = [
-    {{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
-      {
-        id: {{ $index }},
-        href: "{{ .Permalink }}",
-        title: {{ .Title | jsonify }},
-        description: {{ .Params.description | jsonify }},
-        content: {{ .Content | jsonify }}
-      },
-    {{ end -}}
-  ];
-  */
-
-  // https://discourse.gohugo.io/t/range-length-or-last-element/3803/2
-
-  {{ $list := (where .Site.Pages "Section" "docs") -}}
-  {{ $len := (len $list) -}}
-
-  index.add(
-    {{ range $index, $element := $list -}}
-      {
-        id: {{ $index }},
-        href: "{{ .Permalink }}",
-        title: {{ .Title | jsonify }},
-        description: {{ .Params.description | jsonify }},
-        content: {{ .Content | jsonify }}
-      })
-      {{ if ne (add $index 1) $len -}}
-        .add(
-      {{ end -}}
-    {{ end -}}
-  ;
-
-  userinput.addEventListener('input', show_results, true);
-  suggestions.addEventListener('click', accept_suggestion, true);
-
-  function show_results(){
-
-    var value = this.value;
-    var results = index.search(value, { limit: 5, index: ["content"], enrich: true });
-    var entry, childs = suggestions.childNodes;
-    var i = 0, len = results.length;
-
-    suggestions.classList.remove('d-none');
-
-    results.forEach(function(results) {
-
-      entry = document.createElement('div');
-
-      entry.innerHTML = '<a href><span></span><span></span></a>';
-
-      a = entry.querySelector('a'),
-      t = entry.querySelector('span:first-child'),
-      d = entry.querySelector('span:nth-child(2)');
-
-      // console.log(results);
-
-      a.href = results.result[i].doc.href;
-      t.textContent = results.result[i].doc.title;
-      d.textContent = results.result[i].doc.description;
-
-      suggestions.appendChild(entry);
-
-    });
-
-    while(childs.length > len){
-
-        suggestions.removeChild(childs[i])
-    }
-
-  }
-
-  function accept_suggestion(){
-
-      while(suggestions.lastChild){
-
-          suggestions.removeChild(suggestions.lastChild);
-      }
-
-      return false;
-  }
-
-}());
+// 搜尋功能 站不開啟
+//
+// /*
+// Source:
+//   - https://github.com/nextapps-de/flexsearch#index-documents-field-search
+//   - https://raw.githack.com/nextapps-de/flexsearch/master/demo/autocomplete.html
+// */
+//
+// (function(){
+//   var index = new FlexSearch.Document({
+//     tokenize: "forward",
+//     cache: 100,
+//     document: {
+//       id: 'id',
+//       store: [
+//         "href", "title", "description"
+//       ],
+//       index: ["title", "description", "content"]
+//     }
+//   });
+//
+//
+//   // Not yet supported: https://github.com/nextapps-de/flexsearch#complex-documents
+//
+//   /*
+//   var docs = [
+//     {{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
+//       {
+//         id: {{ $index }},
+//         href: "{{ .Permalink }}",
+//         title: {{ .Title | jsonify }},
+//         description: {{ .Params.description | jsonify }},
+//         content: {{ .Content | jsonify }}
+//       },
+//     {{ end -}}
+//   ];
+//   */
+//
+//   // https://discourse.gohugo.io/t/range-length-or-last-element/3803/2
+//
+//   {{ $list := (where .Site.Pages "Section" "paneer_butter_masala") -}}
+//   {{ $len := (len $list) -}}
+//
+//   index.add(
+//     {{ range $index, $element := $list -}}
+//       {
+//         id: {{ $index }},
+//         href: "{{ .Permalink }}",
+//         title: {{ .Title | jsonify }},
+//         description: {{ .Params.description | jsonify }},
+//         content: {{ .Content | jsonify }}
+//       })
+//       {{ if ne (add $index 1) $len -}}
+//         .add(
+//       {{ end -}}
+//     {{ end -}}
+//   ;
+//
+//   userinput.addEventListener('input', show_results, true);
+//   suggestions.addEventListener('click', accept_suggestion, true);
+//
+//   function show_results(){
+//
+//     var value = this.value;
+//     var results = index.search(value, { limit: 5, index: ["content"], enrich: true });
+//     var entry, childs = suggestions.childNodes;
+//     var i = 0, len = results.length;
+//
+//     suggestions.classList.remove('d-none');
+//
+//     results.forEach(function(results) {
+//
+//       entry = document.createElement('div');
+//
+//       entry.innerHTML = '<a href><span></span><span></span></a>';
+//
+//       a = entry.querySelector('a'),
+//       t = entry.querySelector('span:first-child'),
+//       d = entry.querySelector('span:nth-child(2)');
+//
+//       a.href = results.result[i].doc.href;
+//       t.textContent = results.result[i].doc.title;
+//       d.textContent = results.result[i].doc.description;
+//
+//       suggestions.appendChild(entry);
+//
+//     });
+//
+//     while(childs.length > len){
+//
+//         suggestions.removeChild(childs[i])
+//     }
+//
+//   }
+//
+//   function accept_suggestion(){
+//
+//       while(suggestions.lastChild){
+//
+//           suggestions.removeChild(suggestions.lastChild);
+//       }
+//
+//       return false;
+//   }
+//
+// }());
--- content/paneer_butter_masala/recipe/_index.md
+++ content/paneer_butter_masala/recipe/_index.md
@@ -1,6 +1,6 @@
 ---
-title : "Recipe"
-description: "recipe"
+title : "食譜"
+description: "食譜"
 lead: ""
 date: 2020-10-06T08:48:23+00:00
 lastmod: 2020-10-06T08:48:23+00:00
